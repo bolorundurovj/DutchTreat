c59686df2546d2d7c065787e4fddd72b86128cc5
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-01-26 18:54:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace DutchTreat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*What this means to me, it that when the Console program is run,
+             * and  calls the main method, it creates a host, builds, then runs the app */
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+        //This calls the startup class to setup the webhost builder
+    }
+}
